a3bac3b5896ea749f824a25c0a52a7ea9f0db0eb
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2025-03-07 12:52:29 +0200

    Add routing for login and signup pages; update Navbar links

--- frontend-auth-protected/src/components/Navbar.jsx
+++ frontend-auth-protected/src/components/Navbar.jsx
@@ -7,9 +7,13 @@ const Navbar = () => {
         <h1>React Jobs</h1>
       </Link>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/add-job">Add Job</a>
+        <Link to="/">Home</Link>
+        <Link to="/add-job">Add Job</Link>
       </div>
+        <div>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Signup</Link>
+    </div>
     </nav>
   );
 }
